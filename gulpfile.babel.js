669e9aa0d721a4d9549c0bd57a36c5c814004d0c
JINHO SON <son199@hanmail.net>
2020-07-02 03:33:55 +0900

    add animation css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,7 @@ import gulp from "gulp";
 import del from "del";
 import sass from "gulp-sass";
 import minify from "gulp-csso";
-import bro from "gulp-bro";
 import babelify from "babelify";
-import ghPages from "gulp-gh-pages";
 import autoprefixer from "gulp-autoprefixer";
 
 sass.compiler = require("node-sass");
@@ -14,11 +12,6 @@ const routes = {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
     dest: "dist/css",
-  },
-  js: {
-    watch: "src/js/*.js",
-    src: "src/js/main.js",
-    dest: "dist/js",
   },
 };
 
@@ -35,25 +28,12 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
-const js = () =>
-  gulp
-    .src(routes.js.src)
-    .pipe(
-      bro({
-        transform: [babelify.configure({ presets: ["@babel/preset-env"] })],
-      })
-    )
-    .pipe(gulp.dest(routes.js.dest));
-
-const deploy = () => gulp.src("dist/**/*").pipe(ghPages());
-
 const watch = () => {
   gulp.watch(routes.css.watch, styles);
-  gulp.watch(routes.js.watch, js);
 };
 
 const clean = () => del(["dist/styles.css"]);
 const prepare = gulp.series([clean]);
-const assets = gulp.series([styles, js]);
+const assets = gulp.series([styles]);
 const live = gulp.parallel([watch]);
 export const dev = gulp.series([prepare, assets, live]);
